e87c76e04a478359c31394a458cd86c242b654cc
Matthew Calabia <matthew.calabia@founderos.com>
2025-04-14 13:32:16 +0800

    code update

---
+++ contentBasedOnLocation.js
@@ -0,0 +1,51 @@
+const mainCountries = [
+  "pr", "az", "uy", "ss", "sk", "cy", "lk", "gt", "jm", "th", "nz", "bg", "lt", "no", "tw", "hk", "es", "au", "ch",
+  "cr", "vn", "de", "ae", "pt", "be", "tr", "ro", "gr", "fr", "gb", "sg", "za", "hu", "lb", "ar", "ie", "cz", "se",
+  "bo", "us", "sa", "bd", "ca", "mx", "dk", "br", "nl"
+];
+
+const downSellCountries = [
+  "pa", "pl", "pk", "ph", "co", "jp", "it", "ng", "in", "id", "my", "ni", "ec", "hr"
+];
+
+const optionCountries = [
+  "at", "ke", "fi", "ma", "eg", "il", "bb", "cl", "rs", "mk", "do", "pe", "lv", "ee", "ve", "et", "cm", "mt", "dz",
+  "ug", "si", "qa", "gh", "kr", "ru", "lu", "jo", "ua", "rw", "om", "ge", "zw", "tt", "tz", "mu", "np", "mw", "kw",
+  "is", "dm", "ky", "bj", "bs", "aw", "uz", "py", "na", "ad", "al", "sr", "sv", "cw", "bh", "zm", "tn", "mm", "me",
+  "md", "li", "je", "hn", "gu", "cn", "kh", "bt", "vi", "ne", "mn", "mv", "mg", "kz", "iq", "ga", "sz", "ci", "bn",
+  "bw", "ba", "bz", "by", "am"
+];
+
+const headerTextElement = document.getElementById('home-header');
+
+function getCountryCode() {
+  return fetch('https://ipapi.co/country_code/')
+    .then(response => response.text())
+    .then(code => code.toLowerCase())
+    .catch(error => {
+      console.error('Error fetching country:', error);
+      return null;
+    });
+}
+
+async function updateHeaderText() {
+  const currentCode = await getCountryCode();
+  console.log('Current Country Code:', currentCode);
+
+  if (currentCode && mainCountries.includes(currentCode)) {
+    document.getElementById('main').style.display = 'block';
+    document.getElementById('down-sell').style.display = 'none';
+    document.getElementById('option').style.display = 'none';
+  } else if (currentCode && downSellCountries.includes(currentCode)) {
+    document.getElementById('main').style.display = 'none';
+    document.getElementById('down-sell').style.display = 'block';
+    document.getElementById('option').style.display = 'none';
+    headerTextElement.textContent = 'Down sell';
+  } else if (currentCode && optionCountries.includes(currentCode)) {
+    document.getElementById('main').style.display = 'none';
+    document.getElementById('down-sell').style.display = 'none';
+    document.getElementById('option').style.display = 'block';
+  }
+}
+
+updateHeaderText();
---
+++ script-backup.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+    
+    function handleFormSubmission(formClassName) {
+        $(formClassName).submit(function (e) {
+            e.preventDefault();
+            var form = this;
+
+            // Get form field values
+            var name = form.querySelector('input[name="first_name"]').value;
+            var email = form.querySelector('input[name="email_address"]').value;
+            var phone = form.querySelector('input[name="phone"]').value;
+
+            // Log form data (for debugging)
+            console.log(name + " " + email + " " + phone);
+            var redirectUrl = `https://www.founderos.com/workshop-survey?first_name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}&phone=${encodeURIComponent(phone)}`;
+            window.location.href = redirectUrl;
+        });
+    }
+    handleFormSubmission(".workshop-form-hubspot");
+
+    // Function to play the video by resetting the iframe src
+    function playVideo(iframe) {
+        var iframeSrc = iframe.attr('src');
+        var iframeClass = "."+iframe.attr('class')+", .video-wrapper, div";
+        console.log(iframe);
+        console.log(iframeClass);
+        // iframe.attr('src', iframeSrc + '?autoplay=1');  
+    }
+
+    // Function to pause the video by resetting the iframe src
+    function pauseVideo(iframe) {
+        var iframeSrc = iframe.attr('src').split('?')[0]; // Get the base URL of the iframe
+        iframe.attr('src', iframeSrc);  // Remove the autoplay parameter to stop the video
+    }
+
+    $('.sw1').on('click', function() {
+        var iframe = $('.testimonial-video-1');
+        playVideo(iframe);
+    });
+
+    $('.sw2').on('click', function() {
+        var iframe = $('.testimonial-video-2');
+        playVideo(iframe);
+    });
+
+    $('.sw3').on('click', function() {
+        var iframe = $('.testimonial-video-3');
+        playVideo(iframe);
+    });
+
+    $('.sw4').on('click', function() {
+        var iframe = $('.testimonial-video-4');
+        playVideo(iframe);
+    });
+
+    $('.sw5').on('click', function() {
+        var iframe = $('.testimonial-video-5');
+        playVideo(iframe);
+    });
+
+    // When ".close-modal" or ".x-modal" is clicked, pause the respective video
+    $('.close-modal, .x-modal').on('click', function() {
+        $('iframe.testimonial-video-1, iframe.testimonial-video-2, iframe.testimonial-video-3, iframe.testimonial-video-4, iframe.testimonial-video-5').each(function() {
+            pauseVideo($(this));  // Pause each video on the page
+        });
+    });
+
+
+
+    var iframe = $('iframe.testimonial-video-1')[0];
+
+    if (iframe) { // Check if the iframe exists
+        iframe.onload = function () {
+            var iframeWindow = iframe.contentWindow;
+            if (iframeWindow) {
+                iframeWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+            } else {
+                console.error("Iframe contentWindow is not accessible.");
+            }
+        };
+    } else {
+        console.error("Iframe with class .testimonial-video-1 not found.");
+    }
+
+
+});
